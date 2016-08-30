ac47fd0990e4372cc693727cae30dc28264378d3
Ovi Sofariu <ovi@sofariu.com>
2016-08-30 18:18:31 -0400

    added the rest of the roman numerals for int-to-roman coversion

--- src/test_int_to_roman.c
+++ src/test_int_to_roman.c
@@ -6,8 +6,18 @@ START_TEST (test_int_to_single_roman_letter)
 {
   char expectedNumeral[50] = "";
   ck_assert_str_eq("I", roman_numeral(1, expectedNumeral));
-  ck_assert_str_eq("M", roman_numeral(1000, expectedNumeral));
+  ck_assert_str_eq("IV", roman_numeral(4, expectedNumeral));
+  ck_assert_str_eq("V", roman_numeral(5, expectedNumeral));
+  ck_assert_str_eq("IX", roman_numeral(9, expectedNumeral));
+  ck_assert_str_eq("X", roman_numeral(10, expectedNumeral));
+  ck_assert_str_eq("XL", roman_numeral(40, expectedNumeral));
+  ck_assert_str_eq("L", roman_numeral(50, expectedNumeral));
+  ck_assert_str_eq("XC", roman_numeral(90, expectedNumeral));
+  ck_assert_str_eq("C", roman_numeral(100, expectedNumeral));
+  ck_assert_str_eq("CD", roman_numeral(400, expectedNumeral));
+  ck_assert_str_eq("D", roman_numeral(500, expectedNumeral));
   ck_assert_str_eq("CM", roman_numeral(900, expectedNumeral));
+  ck_assert_str_eq("M", roman_numeral(1000, expectedNumeral));
 }
 END_TEST
 
