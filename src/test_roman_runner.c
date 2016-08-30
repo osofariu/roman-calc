df1435a7ea51e4f1da0e39c1f7a159ec0fbe53f5
Ovi Sofariu <ovi@sofariu.com>
2016-08-30 12:21:54 -0400

    cleaned-up test_roman_to_int to separate tests

--- src/test_roman_runner.c
+++ src/test_roman_runner.c
@@ -12,20 +12,3 @@ int main(int argc, char* argv[]) {
     srunner_free(sr);
     return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
  }
-
-
-/*
--int main(int argc, char* argv[]) {
--    int number_failed;
--    Suite *s;
--    SRunner *sr;
--
--    s = roman_numerals_suite();
--    sr = srunner_create(s);
--
--    srunner_run_all(sr, CK_NORMAL);
--    number_failed = srunner_ntests_failed(sr);
--    srunner_free(sr);
--    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
-- }
- */
--- src/test_roman_to_int.c
+++ src/test_roman_to_int.c
@@ -2,23 +2,32 @@
 #include <stdlib.h>
 #include "roman.h"
 
-START_TEST (test_roman_values_additive)
+START_TEST (test_roman_values)
 {
   ck_assert_int_eq(1, roman_value("I"));
-  ck_assert_int_eq(2, roman_value("II"));
-  ck_assert_int_eq(3, roman_value("III"));
   ck_assert_int_eq(5, roman_value("V"));
   ck_assert_int_eq(10, roman_value("X"));
   ck_assert_int_eq(50, roman_value("L"));
   ck_assert_int_eq(100, roman_value("C"));
   ck_assert_int_eq(500, roman_value("D"));
-  ck_assert_int_eq(1000, roman_value("Mxs"));
+  ck_assert_int_eq(1000, roman_value("M"));
+}
+END_TEST
+
+START_TEST (test_roman_values_additive)
+{
+  ck_assert_int_eq(2, roman_value("II"));
+  ck_assert_int_eq(3, roman_value("III"));
+  ck_assert_int_eq(6, roman_value("VI"));
+  ck_assert_int_eq(1000, roman_value("Mxs")); // TODO add test to fix this (invalid)
 }
 END_TEST
 
 START_TEST (test_roman_values_lesser_subtract)
 {
   ck_assert_int_eq(4, roman_value("IV"));
+  ck_assert_int_eq(9, roman_value("IX"));
+  ck_assert_int_eq(99, roman_value("IC"));  // TODO add test to fix this (invalid)
 }
 END_TEST
 
@@ -29,6 +38,7 @@ Suite * roman_to_int_suite(void) {
     s = suite_create("Roman Numerals");
     tc_core = tcase_create("roman-values");
 
+    tcase_add_test(tc_core, test_roman_values);
     tcase_add_test(tc_core, test_roman_values_additive);
     tcase_add_test(tc_core, test_roman_values_lesser_subtract);
     suite_add_tcase(s, tc_core);
