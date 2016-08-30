abfc48a3b6ce6eb4e41d2c4cfa764d24d3896028
Ovi Sofariu <ovi@sofariu.com>
2016-08-30 18:09:41 -0400

    eliminated unnecessary if-statements to simplify algorithm

--- src/int_to_roman.c
+++ src/int_to_roman.c
@@ -2,23 +2,27 @@
 #include "int_to_roman.h"
 #include "roman.h"
 
+struct RomanNumeral {
+  int value;
+  char *chars;
+};
+
+const struct RomanNumeral romanNumerals[] = {
+  {1000, "M"}, {900, "CM"}, {1, "I"}
+};
+
 char *roman_numeral(int numeralValue, char* romanNumeralOutput) { 
   char *outputPtr = romanNumeralOutput;
   while (numeralValue > 0) {
-    if (numeralValue >= 1000) {
-      strcpy(outputPtr, "M");
-      outputPtr += 1;
-      numeralValue -= 1000;
-    }
-    else if (numeralValue >= 900) {
-      strcpy(outputPtr, "CM");
-      outputPtr += 2;
-      numeralValue -= 900;
-    }
-    else if (numeralValue >= 1) {
-      strcpy(outputPtr, "I");
-      outputPtr +=1;
-      numeralValue -= 1;
+    int i;
+    int max_i = sizeof(romanNumerals)/sizeof(romanNumerals[0]);
+    for (i = 0; i < max_i; i++) {
+      struct RomanNumeral currentNumeral = romanNumerals[i];
+      if (numeralValue >= currentNumeral.value) {
+        strcpy(outputPtr, currentNumeral.chars);
+        outputPtr += strlen(currentNumeral.chars);
+        numeralValue -= currentNumeral.value;
+      }
     }
   }
   
